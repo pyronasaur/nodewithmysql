4bf3936beddc09ed2506fce7086d6faf4b0617cf
pyronasaur <pyronnicholas@gmail.com>
2019-10-11 16:49:18 -0400

    Application working and accepting input, updating to db

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodewithmysql
+
+This project is a command line application that uses mysql and inquirer node packages to track customer purchases of inventory.  A customer selects the item they want by id, and selects the quantity to purchase.  If there is sufficient quantity of the item, the database is then updated with the changes to stock and the customer is given the price of their order.
+
+Images can be found in the img folder.
+
+
+1.  ![Initial Stock Population](./img/InitialQueryPopulate.PNG)
+2.  ![Initial Baseball quantity (item to test purchase)](./img/InitialBaseballQuantity.PNG)
+3.  ![Selection options for user view](./img/InitialQueryPopulate2.PNG)
+4.  ![View of purchase complete with purchase price displayed and quantity change in database.](./img/FullSnapQuantityChanged.PNG)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -51,134 +51,38 @@ function gatherQuantity(id) {
     })
     .then(function(answer) {
       var query = "SELECT stock_quantity FROM products WHERE item_id = " + id;
-      console.log(query);
-      console.log(id);
+      //console.log(query);
+      //console.log(id);
       connection.query(query, function(err, res) {
-        console.log(res[0].stock_quantity + "" + answer.quantity)
+        //console.log(res[0].stock_quantity + "" + answer.quantity)
         if(res[0].stock_quantity < answer.quantity) {
           console.log(`Insufficient quantity!`);
           gatherInfo();
         }
         else{
-
+          var quantityToReduce = res[0].stock_quantity - answer.quantity;
+          reduceQuantity(quantityToReduce, id, answer.quantity);
         }
-        //gatherInfo();
       });
     });
 }
 
-function multiSearch() {
-  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+function reduceQuantity(quantityToReduce, id, quanPurchased) {
+  var query = "UPDATE products SET stock_quantity = "+ quantityToReduce +" WHERE item_id = " + id;
   connection.query(query, function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].artist);
-    }
-    runSearch();
+    console.log("Stock has been updated to reflect recent purchases.")
+    valueOfPurchase(quanPurchased, id);
   });
 }
 
-function rangeSearch() {
-  inquirer
-    .prompt([
-      {
-        name: "start",
-        type: "input",
-        message: "Enter starting position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      },
-      {
-        name: "end",
-        type: "input",
-        message: "Enter ending position: ",
-        validate: function(value) {
-          if (isNaN(value) === false) {
-            return true;
-          }
-          return false;
-        }
-      }
-    ])
-    .then(function(answer) {
-      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
-      connection.query(query, [answer.start, answer.end], function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            "Position: " +
-              res[i].position +
-              " || Song: " +
-              res[i].song +
-              " || Artist: " +
-              res[i].artist +
-              " || Year: " +
-              res[i].year
-          );
-        }
-        runSearch();
-      });
-    });
-}
+  function valueOfPurchase(quantityPurchased, id) {
+    var query = "SELECT price FROM products WHERE item_id = " + id;
+    connection.query(query, function(err, res) {
+      console.log(res[0].price + " " + id + " " + quantityPurchased);
+      var priceOfPurchase = quantityPurchased * res[0].price;
+        console.log("Thank you for your purchase.  You have been charged for $" + priceOfPurchase);
+        gatherInfo();
+      })     
+  }
 
-function songSearch() {
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to look for?"
-    })
-    .then(function(answer) {
-      console.log(answer.song);
-      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
-        console.log(
-          "Position: " +
-            res[0].position +
-            " || Song: " +
-            res[0].song +
-            " || Artist: " +
-            res[0].artist +
-            " || Year: " +
-            res[0].year
-        );
-        runSearch();
-      });
-    });
-}
 
-function songAndAlbumSearch() {
-  inquirer
-    .prompt({
-      name: "artist",
-      type: "input",
-      message: "What artist would you like to search for?"
-    })
-    .then(function(answer) {
-      var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
-      query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
-      query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
-
-      connection.query(query, [answer.artist, answer.artist], function(err, res) {
-        console.log(res.length + " matches found!");
-        for (var i = 0; i < res.length; i++) {
-          console.log(
-            i+1 + ".) " +
-              "Year: " +
-              res[i].year +
-              " Album Position: " +
-              res[i].position +
-              " || Artist: " +
-              res[i].artist +
-              " || Song: " +
-              res[i].song +
-              " || Album: " +
-              res[i].album
-          );
-        }
-
-        runSearch();
-      });
-    });
-}
